bcb59f89e7d7eb8bb730d8246e75d64989a20e72
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-06 16:11:25 +0300

    last commit, finished everything

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const replaceTemplate = require(`${__dirname}/modules/replaceTemplate.js`)
+const slugify = require('slugify');
+const replaceTemplate = require(`${__dirname}/modules/replaceTemplate.js`);
 
 //////////////////////////////
 // FILES
@@ -31,6 +32,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true}))
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
 
